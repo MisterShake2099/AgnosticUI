0296a67e3b9124ffb39c07bdc50e62943188a08c
Doug <mistershake2099@gmail.com>
2019-01-28 15:40:35 -0800

    Init Git. Fair bit of work already done.

---
+++ AgnosticUI/Assets/Scripts/UI_Screen.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace DevName.UI
+{
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(CanvasGroup))]
+	public class UI_Screen : MonoBehaviour
+	{
+		#region Variables
+		[Header("Main Properties")]
+		public Selectable m_StartSelectable;
+
+		[Header("Screen Events")]
+		public UnityEvent OnScreenStart = new UnityEvent();
+		public UnityEvent OnScreenClose = new UnityEvent();
+
+		private Animator animator;
+		#endregion
+
+		#region Main Methods
+		private void Start()
+		{
+			animator = GetComponent<Animator>();
+
+			// Make assigned Selectable "selected" on startup.
+			if (m_StartSelectable)
+			{
+				EventSystem.current.SetSelectedGameObject(m_StartSelectable.gameObject);
+			}
+		}
+		#endregion
+
+		#region Helper Methods
+		public virtual void StartScreen()
+		{
+			if (OnScreenStart != null)
+			{
+				OnScreenStart.Invoke();
+			}
+
+			HandleAnimator("show");
+		}
+
+		public virtual void CloseScreen()
+		{
+			if (OnScreenClose != null)
+			{
+				OnScreenClose.Invoke();
+			}
+
+			HandleAnimator("hide");
+		}
+
+		void HandleAnimator(string trigger)
+		{
+			if (animator)
+			{
+				animator.SetTrigger(trigger);
+			}
+		}
+		#endregion
+	}
+}
---
+++ AgnosticUI/Assets/Scripts/UI_System.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DevName.UI
+{
+	public class UI_System : MonoBehaviour
+	{
+
+		#region Variables
+		[Header("System Events")]
+		public UnityEvent onSwitchedScreen = new UnityEvent();
+
+		private Component[] screens = new Component[0];
+		private UI_Screen currentScreen;
+		public UI_Screen CurrentScreen { get { return currentScreen; } }
+		private UI_Screen previousScreen;
+		public UI_Screen PreviousScreen { get { return previousScreen; } }
+		#endregion
+
+
+		#region Main Methods
+		#endregion
+		private void Start()
+		{
+			screens = GetComponentsInChildren<UI_Screen>(true);
+		}
+
+
+		#region Helper Methods
+		public void SwitchScreen(UI_Screen screen)
+		{
+			if (screen)
+			{
+				if (currentScreen)
+				{
+					//currentScreen.Close();
+					previousScreen = currentScreen;
+				}
+
+				currentScreen = screen;
+				currentScreen.gameObject.SetActive(true);
+				//currentScreen.StartScreen();
+
+				if (onSwitchedScreen != null)
+				{
+					onSwitchedScreen.Invoke();
+				}
+			}
+		}
+
+		public void GoToPreviousScreen()
+		{
+			if (previousScreen)
+			{
+				SwitchScreen(previousScreen);
+			}
+		}
+
+		public void LoadScene(int sceneIndex)
+		{
+			StartCoroutine(WaitToLoadScene(sceneIndex));
+		}
+
+		IEnumerator WaitToLoadScene(int sceneIndex)
+		{
+			yield return null;
+		}
+		#endregion
+
+	}
+}
